8565bf376d35c286e909454cba84dd5c37cb46e6
Konstantin Tieber <kt@webfactory.de>
2018-08-30 12:33:14 -0400

    Refactor Favorites component for increased readability and maintainability

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -11,137 +11,48 @@ const Favorites = (props) => {
         return (<p>No favorites saved.</p>)
     }
 
-    const thursdayFavorites = props.items.filter(item => item.day === "Thursday"); // no sorting required since there is only one keynote
-    const fridayFavorites = props.items
-                                .filter(item => item.day === "Friday")
-                                .sort((item1, item2) => item1.id > item2.id);
-    const saturdayFavorites = props.items
-                                .filter(item => item.day === "Saturday")
-                                .sort((item1, item2) => item1.id > item2.id);
-    const sundayFavorites = props.items
-                                .filter(item => item.day === "Sunday")
-                                .sort((item1, item2) => item1.id > item2.id);
+    const favorites = props.days.map(day => {
+        const talksOnDay = props.items
+                            .filter(talk => talk.day === day)
+                            .sort((talk1, talk2) => talk1.id > talk2.id);
+
+        if (talksOnDay.length === 0) {
+            return '';
+        }
+
+        return (
+            <Fragment key={day}>
+                <Header as='h2'>{day}</Header>
+                <List divided relaxed verticalAlign='middle' size="large">
+                    {talksOnDay.map(talk => (
+                        <ScheduleItem
+                            key={talk.id}
+                            id={talk.id}
+                            speaker={talk.speaker}
+                            name={talk.name}
+                            starred={true}
+                            time={talk.time}
+                            starHandler={props.removeStarHandler}
+                            location={talk.location}
+                            info={talk.info} />
+                    ))}
+                </List>
+            </Fragment>
+        )
+    });
 
     return (
         <Fragment>
-            {thursdayFavorites.length ?
-                <Fragment>
-                    <Header as='h2'>Thursday</Header>
-                    <List divided relaxed verticalAlign='middle' size="large">
-                        {thursdayFavorites.map(talk => (
-                            <ScheduleItem
-                                key={talk.id}
-                                id={talk.id}
-                                speaker={talk.speaker}
-                                name={talk.name}
-                                starred={true}
-                                time={talk.time}
-                                starHandler={props.removeStarHandler}
-                                location={talk.location}
-                                info={talk.info} />)
-                        )
-                        }
-                    </List>
-                </Fragment>
-                : '' }
-            {fridayFavorites.length ?
-                <Fragment>
-                    <Header as='h2'>Friday</Header>
-                    <List divided relaxed verticalAlign='middle' size="large">
-                        {fridayFavorites.map(talk => (
-                            <ScheduleItem
-                                key={talk.id}
-                                id={talk.id}
-                                speaker={talk.speaker}
-                                name={talk.name}
-                                starred={true}
-                                time={talk.time}
-                                starHandler={props.removeStarHandler}
-                                location={talk.location}
-                                info={talk.info} />)
-                            )
-                        }
-                    </List>
-                </Fragment>
-            : '' }
-            {saturdayFavorites.length ?
-                <Fragment>
-                    <Header as='h2'>Saturday</Header>
-                    <List divided relaxed verticalAlign='middle' size="large">
-                        {saturdayFavorites.map(talk => (
-                            <ScheduleItem
-                                key={talk.id}
-                                id={talk.id}
-                                speaker={talk.speaker}
-                                name={talk.name}
-                                starred={true}
-                                time={talk.time}
-                                starHandler={props.removeStarHandler}
-                                location={talk.location}
-                                info={talk.info} />)
-                            )
-                        }
-                    </List>
-                </Fragment>
-            : '' }
-            {sundayFavorites.length ?
-                <Fragment>
-                    <Header as='h2'>Sunday</Header>
-                    <List divided relaxed verticalAlign='middle' size="large">
-                        {sundayFavorites.map(talk => (
-                            <ScheduleItem
-                                key={talk.id}
-                                id={talk.id}
-                                speaker={talk.speaker}
-                                name={talk.name}
-                                starred={true}
-                                time={talk.time}
-                                starHandler={props.removeStarHandler}
-                                location={talk.location}
-                                info={talk.info} />)
-                        )
-                        }
-                    </List>
-                </Fragment>
-                : '' }
+            {favorites}
         </Fragment>
     )
 };
 
 Favorites.propTypes = {
     items: PropTypes.array.isRequired,
+    days: PropTypes.array.isRequired,
     removeStarHandler: PropTypes.func.isRequired
 };
 
 
 export default Favorites
-
-/**
- * Notes for Refactoring:
- * const getFavoriteItemsOnDay = (day, items) => {
-        const filteredItems = items.filter(item => item.day === "Friday").sort((item1, item2) => item1.id > item2.id);
-
-        let listElements = [];
-        filteredItems.forEach((talk) => {
-            listElements.push(<ScheduleItem
-                key={talk.id}
-                id={talk.id}
-                speaker={talk.speaker}
-                name={talk.name}
-                starred={true}
-                time={talk.time}
-                starHandler={props.removeStarHandler}
-                location={talk.location}
-                info={talk.info} />)
-        });
-
-        return listElements;
-    };
-
- const favorites = props.days.map(day => {
-        let dayFavoritesJsx = [];
-        dayFavoritesJsx.push(<Header as='h2'>{day}</Header>);
-        dayFavoritesJsx.push(<List divided relaxed verticalAlign='middle' size="large">{getFavoriteItemsOnDay(day, props.items)}</List>);
-        return dayFavoritesJsx
-    });
- */
